389d02aa9f86d8007bef95496d4ce61aad6f0c7f
Meenaz Momin <meenaz.riyaz@apsissolutions.com>
2024-01-12 16:31:26 +0530

    forgot password apis are done, sending email generate otp verify otp

--- backend/portal/services.py
+++ backend/portal/services.py
@@ -1,14 +1,14 @@
-# import threading
+import threading
 from django.core.mail import EmailMessage
 from django.conf import settings
 
-# class EmailThread(threading.Thread):
-#     def __init__(self, email):
-#         self.email = email
-#         threading.Thread.__init__(self)
+class EmailThread(threading.Thread):
+    def __init__(self, email):
+        self.email = email
+        threading.Thread.__init__(self)
 
-#     def run(self):
-#         self.email.send(fail_silently=True)
+    def run(self):
+        self.email.send(fail_silently=True)
 
 
 def trigger_mails(subject, body, recievers, to_cc):
@@ -19,5 +19,4 @@ def trigger_mails(subject, body, recievers, to_cc):
         list(set(recievers)),  # recievers
         cc=list(set(to_cc)),  # in cc
     )
-    email.send(fail_silently=True)
-    # EmailThread(email).start()
+    EmailThread(email).start()
